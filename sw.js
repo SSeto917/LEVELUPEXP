c45afcd2c30e88a3ae1cc9a9d982cd439c954972
SSeto917 <setosaori30616@gmail.com>
2026-09-21 02:11:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="operation-growth-pwa-v23-pixel-icons";
+const CACHE="operation-growth-pwa-v27-purge-done-challenges";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
